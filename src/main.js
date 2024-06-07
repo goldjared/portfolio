66876920cbe1e9fa806b360ac15237f9e16f4015
jared <114547220+goldjared@users.noreply.github.com>
2024-06-06 22:38:06 -0400

    prevent page content going under fixed header

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 const currentPage = (() => {
-  let currentPathArr = window.location.pathname.split("/");
-  let currentPage = currentPathArr[currentPathArr.length-1].split(".")[0];
+  let currentPathArr = window.location.pathname.split('/');
+  let currentPage = currentPathArr[currentPathArr.length - 1].split('.')[0];
   return currentPage;
 })();
-
 
 const navBar = document.querySelectorAll('.page-link');
 
 const navStyle = (() => {
-let navStyleIndex = 0;
-if(currentPage === "projects") {
-  navStyleIndex = 1;
-} else if(currentPage === "posts") {
-  navStyleIndex = 2;
-}
+  let navStyleIndex = 0;
+  if (currentPage === 'projects') {
+    navStyleIndex = 1;
+  } else if (currentPage === 'posts') {
+    navStyleIndex = 2;
+  }
 
-const selectedNav = navBar[0].children[navStyleIndex];
-selectedNav.style.borderBottom = "solid 5px #902639";
-selectedNav.style.borderRadius = "5px";
+  const selectedNav = navBar[0].children[navStyleIndex];
+  selectedNav.style.borderBottom = 'solid 5px #902639';
+  selectedNav.style.borderRadius = '5px';
 })();
 
+// prevent content going under fixed header
+let header = document.querySelector('header-component');
+let proj = document.querySelector('.project-container');
+proj.style.marginTop = header.offsetHeight + 'px';
